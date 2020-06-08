fe115469e82e3e8893e934f29b3a24d7295e8362
digvijaysrathore <dynamicdigvijay@gmail.com>
2020-06-08 11:24:22 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -16,6 +16,9 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
+    bio: {
+        type: String
+    },
     docs: [],
     upvote: {
         type: Number,
